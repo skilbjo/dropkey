414b45e060a93c2dc5ea4515611afe514e58c501
skilbjo <john.skilbeck@gmail.com>
2014-12-29 16:53:54 -0800

    stuff

--- server.js
+++ server.js
@@ -1,16 +1,61 @@
 var 
-	dbox = require('dbox')
-	, dropbox = dbox.app({'app_key': process.env.DROPBOX_KEY, 'app_secret': process.env.DROPBOX_SECRET})
-	, client;
+  http            = require('http')
+  , flash           = require('connect-flash')
+  , express         = require('express')
+  , app             = express()
+  , morgan          = require('morgan')
+  , favicon         = require('serve-favicon')
+  , cookieParser    = require('cookie-parser')
+  , methodOverride  = require('method-override')
+  , session         = require('express-session')
+  , errorHandler    = require('errorhandler')
+  , bodyParser      = require('body-parser')
+  , busboy          = require('connect-busboy')
+  , marked          = require('marked').setOptions({ breaks: true })
+  , db              = require('./app/model/index.js')
+  , env             = (process.env.NODE_ENV || 'development');
 
-dropbox.requesttoken( function(status, request_token) {
-	dropbox.accesstoken(request_token, function(status, access_token){
-		console.log(access_token);
-		client = dropbox.client(access_token);
-		client.account(function(status, reply) {
-			console.log(reply);
-		});
-	});
+// configuration ==============
+  // boilerplate
+app.use(cookieParser()); app.use(methodOverride()); app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json()); app.use(errorHandler()); app.use(morgan('dev'));  app.use(flash()); app.use(busboy());
+
+app.set('port', process.env.PORT || 8080);
+// app.use(favicon(__dirname + '/public/src/assets/favicon/favicon.ico'));
+app.use('/public', express.static('public'));
+app.use('/bower', express.static('bower_components'));
+
+// view template engine
+app.set('view engine', 'jade');
+app.set('views', __dirname + '/app/view');
+
+// models
+app.set('models', require('./app/model'));
+
+// MVC Definitions =============
+// models =============
+var model = { 
+  // cost        : app.get('models').Cost
+};
+
+// controllers ========
+var controller = {
+  static_pages    : require('./app/controller/static_pages.js') 
+  , dropbox       : require('./app/controller/dropbox.js')
+};
+
+// routes =============
+require('./app/routes.js')(app
+  , model
+  , controller
+  );
+
+// launch ===================
+db.sequelize.sync({ force: false }).complete(function(err) {
+  if (err) { throw err[0] ; } else { 
+    http.createServer(app).listen(app.get('port'), function(){ 
+      console.log('The magic happens on port ' + app.get('port'));
+    });
+  }
 });
-
 
