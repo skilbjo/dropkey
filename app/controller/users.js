5334c7c67c4cce6603c899c8b96cccf3f42399dc
John Skilbeck <john.skilbeck@gmail.com>
2015-09-03 18:21:14 -0700

    Fixed oauth2 / passport / sequelizejs issue with login

--- app/controller/users.js
+++ app/controller/users.js
@@ -11,10 +11,9 @@ exports.index = function(req, res, model) {
 
 exports.show = function(req, res, model) {	
 	var md5 	= require('crypto').createHash('md5');
-	console.log('in controller', req.user.UserId);
 	model.user
-	.find(req.user.dataValues.UserId)
-	.then(function(err, user) {
+	.find(req.user.UserId)
+	.then(function(user) {
 		res.render('users/profile', {
 			name 	: 	req.user.Name,
 			email : 	req.user.Email,
@@ -24,12 +23,10 @@ exports.show = function(req, res, model) {
 	});
 };
 
-
 exports.logout = function (req, res) {
   req.logout();
   res.redirect('/?logout=true');
 };
-
 
 // Stubs
 exports.edit = function(req, res, model) {
@@ -43,8 +40,3 @@ exports.update = function(req, res, model) {
 exports.delete = function(req, res, model) {
 	res.send('stub');
 };
-
-// Obsolete due to Passport.js
-
-// get /user/new handled by passport.js /* exports.new = function(req, res) { res.render('users/new'); }; */
-// post /user handled by passport.js /* exports.create = function(req, res, model) { console.log(req.body); model.user .create({ Name: 	req.body.name, Email: 	req.body.email }) .then(function(user) { if(err || !user) { res.send(err); return; } else { console.log(user); res.send('hay guy nice job'); } }); }; */
--- app/routes.js
+++ app/routes.js
@@ -15,8 +15,7 @@ module.exports = function(app,
     .get(isLoggedIn, function(req, res) { controller.users.index(req, res, model); } );
 
   app.route('/users/:id([0-9]+)')
-    .get(isLoggedIn, function(req, res) { 
-      console.log('get profile', req.user.dataValues.UserId);
+    .get(isLoggedIn, isSameUser, function(req, res) { 
       controller.users.show(req, res, model); 
     });
 
@@ -39,63 +38,21 @@ module.exports = function(app,
   app.route('/auth/dropbox/callback')
   .get(function(req, res) { passport.authenticate('dropbox-oauth2')(req, res, function() {
       res.redirect('/users/' + req.user[0].UserId);
-      // controller.users.show(req, res, model);
     });
   });
-
-
-  // app.get('/auth/dropbox/callback', function(req, res, next) {
-  //   console.log('up here');
-  //   passport.authenticate('dropbox-oauth2', function(err, user, info) {
-  //     console.log('trying to auth');
-  //     if (err) return next(err);
-  //     if (!user) return res.redirect('/');
-  //     req.logIn(user, function(err) {
-  //       if (err) return next(err);
-  //       return res.redirect('/users/' + user.UserId);
-  //     });
-  //   });
-  // });
-
-
-  // // Dropbox
-  // app.route('/auth/dropbox')
-  //   .get( passport.authenticate('dropbox-oauth2') );
-  // app.route('/auth/dropbox/callback')
-  // .get(function(req, res) { passport.authenticate('dropbox-oauth2')(req, res, function() {
-  //     console.log(req.user);
-  //     req.session.save(function() {
-  //       res.send(req.user);
-  //       // res.redirect('/users/' + req.user[0].dataValues.UserId);
-  //     });
-  //   });
-  // });
 
 // Facebook
   app.route('/auth/facebook')
     .get( passport.authenticate('facebook', { scope: "email"} ));
   app.route('/auth/facebook/callback')
     .get(function(req, res) { passport.authenticate('facebook')(req, res, function() {
-        // console.log(req.user);
         res.redirect('/users/' + req.user.UserId);
       });
     });
 
-
-
-  // get  /user/new Create new user handled by Passport.js  /* app.route('/users/new').get(function(req, res) { controller.users.new(req, res, model); } ); */
-  // post /user     Create new user handled by Passport.js  /* app.route('/users').post(function(req, res) { controller.users.create(req, res, model); } ); */
-
-
 // Dropbox ========================
   app.route('/dropbox/new')
     .get( controller.dropbox.new);
-
-   app.route('/request_token')
-    .get( function(req, res) { controller.dropbox.request_token(req, res, env); });
-
-  app.route('/access_token')
-    .get( function(req, res) { controller.dropbox.access_token(req, res); });
 
   app.route('/tree')
     .get( function(req, res) { controller.dropbox.tree(req, res); } );
@@ -107,15 +64,11 @@ module.exports = function(app,
 
 
 function isSameUser(req, res, next) {
-  next();
-  /*
   if (parseInt(req.params.id) === req.user.UserId) {
-    return next();
+    next();
   } else {
-  res.redirect('/users/' + req.user.UserId);
+    res.redirect('/users/' + req.user.UserId);
   }
-  */
-
 }
 
 function isLoggedIn(req, res, next) {
